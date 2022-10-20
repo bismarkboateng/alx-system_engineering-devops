ccc6ebe178e0684f61407eec039f251ef3f1f1c4
bismarkboateng <bismarkb609@gmail.com>
2022-10-20 01:16:14 +0000

    apis

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
-"""Exports data in the JSON format"""
+''' makes a get request to a REST API '''
+import json
+import requests
+from sys import argv
 
-if __name__ == "__main__":
 
-    import json
-    import requests
-    import sys
-
-    userId = sys.argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(userId))
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    todos = todos.json()
-
-    todoUser = {}
-    taskList = []
-
-    for task in todos:
-        if task.get('userId') == int(userId):
-            taskDict = {"task": task.get('title'),
-                        "completed": task.get('completed'),
-                        "username": user.json().get('username')}
-            taskList.append(taskDict)
-    todoUser[userId] = taskList
-
-    filename = userId + '.json'
-    with open(filename, mode='w') as f:
-        json.dump(todoUser, f)
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    user_data = requests.get(url + 'users/{}'.format(argv[1])).json()
+    user_tasks = requests.get(url + 'todos', params={'userId': argv[1]}).json()
+    with open('{}.json'.format(argv[1]), 'w') as f:
+        json.dump({argv[1]: [{
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": user_data.get('username')
+        } for task in user_tasks]}, f)
